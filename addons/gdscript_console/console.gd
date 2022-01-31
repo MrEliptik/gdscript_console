c387fff13b1efc7e15167be651c6f62995659ad3
Victor <victormeunier.dev@gmail.com>
2022-01-31 10:09:07 +0100

    udpate console and plugin to support function calling correctly add zoom

--- addons/gdscript_console/console.gd
+++ addons/gdscript_console/console.gd
@@ -1,16 +1,33 @@
 tool
 extends Control
 
-onready var editor = $VSplitContainer/Editor
-onready var output = $VSplitContainer/VBoxContainer/Result
+onready var editor = $VBoxContainer/VSplitContainer/Editor
+onready var output = $VBoxContainer/VSplitContainer/VBoxContainer/Result
 
 onready var expression = Expression.new()
 
 onready var root_node = get_tree().get_edited_scene_root()
 
+var plugin: EditorPlugin setget set_plugin, get_plugin
+var curr_script: Script
+var curr_node = null
+
+var code_font: DynamicFont
+
+# Used to zoom in and out
+var ctrl_pressed: bool = false
+
 func _ready() -> void:
+	$VBoxContainer/HBoxContainer/Message.text = ""
 	# TODO: change font size to the same as the editor
 	output.add_keyword_color("ERROR", Color(1.0, 0.0, 0.0))
+	
+func set_plugin(val) -> void:
+	plugin = val
+	plugin.connect("script_changed_to", self, "on_script_changed")
+	
+func get_plugin() -> EditorPlugin:
+	return plugin
 
 func _on_Run_pressed() -> void:
 	root_node = get_tree().get_edited_scene_root()
@@ -19,12 +36,13 @@ func _on_Run_pressed() -> void:
 	for line in lines:
 		var error = expression.parse(line, [])
 		if error != OK:
-			# TODO: print the err to the output console, but in red
 			var err_txt = expression.get_error_text()
-#			output.add_color_region(err_txt.substr(0, 1), "\n", Color(1.0, 0.0, 0.0), false)
 			output.text += "ERROR: " + err_txt + "\n"
 			return
-		var result = expression.execute([], root_node, true)
+#		result = expression.execute([], root_node, true)
+#		result = expression.execute([], curr_script, true)
+		var result = expression.execute([], curr_node, true)
+			
 		if not expression.has_execute_failed():
 			output.text += str(result) + "\n"
 			
@@ -37,3 +55,28 @@ func _on_Editor_text_changed() -> void:
 
 func _on_Editor_request_completion() -> void:
 	pass
+
+func on_script_changed(script: Script) -> void:
+	curr_script = script
+#	curr_node = script.get_local_scene()
+	if curr_node:
+		curr_node.free()
+	curr_node = script.new()
+	
+	if !curr_script.is_tool():
+		$VBoxContainer/HBoxContainer/Message.text = "CAREFUL! Script is not a tool. Calling its functions isn't possible."
+	else:
+		$VBoxContainer/HBoxContainer/Message.text = ""
+
+func _on_Editor_gui_input(event: InputEvent) -> void:
+	# Handle ctrl + zoom
+	if event is InputEventKey:
+		if event.scancode == KEY_CONTROL:
+			ctrl_pressed = event.is_pressed()
+	
+	elif event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				code_font.size += 1
+			elif event.button_index == BUTTON_WHEEL_DOWN:
+				code_font.size -= 1
--- addons/gdscript_console/plugin.gd
+++ addons/gdscript_console/plugin.gd
@@ -1,12 +1,31 @@
 tool
 extends EditorPlugin
 
+signal script_changed_to(script)
+
 var console = preload("res://addons/gdscript_console/console.tscn").instance()
+var curr_script: Script
 
 func _enter_tree() -> void:
+	# Update font
+	var code_font: DynamicFont = load("res://addons/gdscript_console/fonts/code_font.tres")
+	code_font.size = get_editor_interface().get_editor_settings().get_setting("interface/editor/code_font_size")
+	
+	get_editor_interface().get_script_editor().connect("editor_script_changed", self, "on_editor_scrip_changed")
+	curr_script = get_editor_interface().get_script_editor().get_current_script()
+	
+	console.plugin = self
+	console.curr_script = curr_script
+	console.curr_node = curr_script.new()
+	console.code_font = code_font
+	
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, console)
 	
 func _exit_tree() -> void:
 	remove_control_from_docks(console)
-	console.queue_free()
+	console.curr_node.free()
+	console.free()
 	
+func on_editor_scrip_changed(script: Script) -> void:
+	curr_script = script
+	emit_signal("script_changed_to", curr_script)
